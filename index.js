d6ab8e60d82df0980d14282dad1f704a10503a1c
unknown <sml65ar@yahoo.com>
2022-02-17 13:59:05 -0300

    mi primer commit

--- index.js
+++ index.js
@@ -1,162 +1,260 @@
 const prev = document.querySelector("#prev");
 const next = document.querySelector("#next");
-mostrarHTMLDetalles = document.querySelector("#detalles-contenedor")
-SeccionPersonajes = document.querySelector("#tarjeta")
-SeccionDetalles = document.querySelector("#detalle")
+const prevLocation= document.querySelector("#prev-location");
+const nextLocation = document.querySelector("#next-location");
+const prevEpisode= document.querySelector("#prev-episode");
+const nextEpisode = document.querySelector("#next-episode");
+
+
+
+const formCtnBarsSearch = document.querySelector("#ctn-bars-search");
+const inputSearch = document.getElementById("#input-search");
+const cerrarInputSearch = document.getElementById("cerrar-busqueda");
+const seccionCharacter = document.getElementById("seccion-character");
+const seccionLocation = document.getElementById("seccion-location");
+const detalleLocation = document.getElementById("detalle-location");
+
+const botonBusqueda = document.getElementById("#busqueda");
+
+
+
+/*botonBusqueda.onclick = () =>{     MALE: ACA SE ME ROMPE TODO**
+    inputSearch.value = "";                                     *
+}                                                               *
+*///////////////////////////////////////////////////////////////
+
+////////**********buscando********************* */
+
+const mostrarBuscador = ()=>{
+    formCtnBarsSearch.style.top = "80px";
+}
+
+const ocultarBuscador = () =>{
+    formCtnBarsSearch.style.top = "-10px";
+    /*inputSearch.value= "";*/   /*ACA QUERIA QUE AL ESCRIBIR EN EL
+                                   INPUT Y DESPUES DE CERRARSE QUE
+                                   QUEDARA EN BLANCO Y NO LO LOGRE*/
+    
+}
+
+
+document.getElementById("lupa").addEventListener("click", mostrarBuscador)
+document.getElementById("cerrar-busqueda").addEventListener("click",ocultarBuscador)
+
+
+/**************FIN BUSCADOR****************/
 
 let paginaActual = 1
 let ultimaPagina = 0
 
-
-
-const buscarPersonajes = () =>{
+const buscarPersonajes = () => {
     fetch(`https://rickandmortyapi.com/api/character?page=${paginaActual}`)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        ultimaPagina = data.info.pages
-        mostrarHTMLPersonajes(data.results)
-        asignarClickTarjetaPersonaje()
-    })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            ultimaPagina = data.info.pages
+            crearTarjetasPersonaje(data.results)
+        })
+
 }
-
-const buscarPersonaje = (id) =>{
-    fetch(`https://rickandmortyapi.com/api/character/?${id}`)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        //crearVistaPersonaje()
-    })
-}
-buscarPersonaje()
-
-const buscarDetalle = (id) =>{
-    fetch(`https://rickandmortyapi.com/api/character/?${id}`)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        //crearVistaDetalle()
-    })
-}
-buscarDetalle()
-
-
-
-
-const asignarClickTarjetaPersonaje = () => {
-    const tarjetas = document.querySelectorAll(".tarjeta")
-    for (let i = 0; i < tarjetas.length; i++) {
-        tarjetas[i].onclick = () => {
-            const idPersonaje = tarjetas[i].dataset.id
-            buscarPersonaje(idPersonaje)
-            console.log(idPersonaje)
-
-            const idDetalle = tarjetas[i].dataset.id
-            buscarDetalle(idDetalle)
-            console.log(idDetalle)
-
-            SeccionPersonajes.style.display = "none"
-
-            if (idPersonaje === idDetalle) {
-                mostrarHTMLDetalles = (data) => {
-                    const DetalleContenedor = document.querySelector("#detalles-contenedor")
-                    const HTML = data.reduce((acc, curr) => {
-                        return acc + `
-                                <div class = "detalle" data-id=${curr.id}>
-                            <h4>${curr.status}</h4>
-                            <h4>${curr.species}</h4>
-                            <h4>${curr.type}</h4>
-                            <h4>${curr.gender}</h4>
-                            <h4>${curr.origin.name}</h4>
-                            <h4>${curr.location.name}</h4>
-                            </div>
-                            `
-                    }, "")
-                    DetalleContenedor.innerHTML = HTML
-                }
-            }
-
-          SeccionDetalles.style.display = "flex"
-
-        }
-    }
-}
-
-const mostrarHTMLPersonajes = (personajes) =>{
-    const tarjetasContenedor = document.querySelector("#tarjetas-contenedor")
-    const html = personajes.reduce((acc, curr) =>{
-        return acc + `
-        <div class = "tarjeta" data-id=${curr.id}>
-        <h2>${curr.name}</h2>
-        <img src="${curr.image}"/>
-        </div>
-        `
-    }, "")
-     tarjetasContenedor.innerHTML = html
-    }
-
-
 buscarPersonajes()
 
-//*****************PAGINACION**********************/
-//next.onclick = () =>{
-//    paginaActual ++
-//    if(paginaActual > ultimaPagina){
-//        next.disabled = true
-//      }
-//      if(paginaActual <= ultimaPagina){
-//        prev.disabled = false
-//    } 
+const crearTarjetasPersonaje = (personaje) => {
+    const tarjetas = document.querySelector("#tarjetas-personajes")
+    const html = personaje.reduce((acc, curr) => {
+        return acc + `
+        <div class ="tarjetas" data-id=${curr.id}>
+        <h2>${curr.name}</h2>
+        <img  class="img-personaje" src="${curr.image}"/>
+        
+          <div  class="detalles" data-id=${curr.id}>
+           <h4>Gender: ${curr.gender}</h4>
+           <h4>Origin: ${curr.origin.name}</h4>
+           <h4>Species: ${curr.species}</h4>
+           <h4>Status: ${curr.status}</h4>
+        </div>
+        </div>
+           `
+    }, "")
+
+    tarjetas.innerHTML = html
+}
+
+let paginaActualName = 1
+let ultimaPaginaName = 0
+
+ formCtnBarsSearch.onsubmit = (e) =>{
+    e.preventDefault();
+    buscarPersonajeFiltrado(inputSearch.value)
+}
+
+const buscarPersonajeFiltrado = (name) =>{
+    fetch(`https://rickandmortyapi.com/api/character/?name=${name}`)
+    .catch((error) =>{                     /*TUVE QUE HACER UN CATCH PORQUE ME 
+                                             MANDABA ERROR 404 Y OTRAS VECES ERROR
+                                             500 - CON ESO SE ARREGLO PERO ME TRABO EN
+                                             LA LINEA 33*/
+        console.log(error)
+    })
+    .then(res => res.json())
+    .then(data => {
+        crearTarjetasPersonaje(data.results)
+        console.log(data.results)
+    })
+}
+buscarPersonajeFiltrado()
+
+
+
+
+
+//****************location***************************/
+let paginaActualLocation = 1
+let ultimaPaginaLocation = 0
+
+
+const buscarLocacion = () => {
+    fetch(`https://rickandmortyapi.com/api/location?page=${paginaActualLocation}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            ultimaPaginaLocation = data.info.pages
+            crearTarjetasLocacion(data.results)
+        })
+
+}
+
+const crearTarjetasLocacion = (locaciones) => {
+    const location= document.querySelector("#tarjetas-locacion")
+    const htmlLocacion = locaciones.reduce((acc, curr) => {
+        return acc + `
+        <div  class ="location" data-id=${curr.id}>
+        <h4>${curr.name}</h4>
+        <img class="img-tierra" src="imagenes/locacion.png" id="img-tierra">
+        <h4>Dimension: ${curr.dimension}</h4>
+        <h4>Residents: ${curr.residentes}</h4>
+        </div>
+           `
+    }, "")
+    nextLocation.onclick = () =>{
+        paginaActualLocation ++
+        if(paginaActualLocation > ultimaPaginaLocation){
+            nextLocation.disabled = true
+        }
+          if(paginaActualLocation <= ultimaPaginaLocation){
+            prevLocation.disabled = false
+        } 
+        
+    buscarLocacion();
+    }
     
-//buscarPersonajes()
-//}
-
-//prev.onclick = () =>{
-//    paginaActual --
-
-//    if(paginaActual < 1){
-//       prev.disabled = true
-//    }
-//    if(paginaActual >= 1){
-//        prev.disabled = false
-//    }
-//    buscarPersonajes()
-//}
-//****************************************************//
+    prevLocation.onclick = () =>{
+        paginaActualLocation --
+       if(paginaActualLocation < 1){
+           prevLocation.disabled = true
+        }
+        if(paginaActualLocation >= 1){
+            prevLocation.disabled = false
+        }
+        buscarLocacion();
+    }
 
 
+    location.innerHTML = htmlLocacion
+}
+
+buscarLocacion()
+
+
+//******************EPISODES**********************/
+
+let paginaActualEpisode = 1
+let ultimaPaginaEpisode = 0
+
+
+const buscarEpisode = () => {
+    fetch(`https://rickandmortyapi.com/api/episode?page=${paginaActualEpisode}`)
+        .then(res => res.json())
+        .then(data => {
+
+            console.log(data)
+            ultimaPaginaEpisode = data.info.pages
+            crearTarjetasEpisode(data.results)
+        })
+
+}
+
+const crearTarjetasEpisode = (episodios) => {
+    const episode= document.querySelector("#tarjetas-episode")
+    const htmlEpisode = episodios.reduce((acc, curr) => {
+        return acc + `
+        <div  class ="episode" data-id=${curr.id}>
+        <h4>${curr.name}</h4>
+        <img class="img-tv" src="imagenes/tv.jpg" id="img-tv">
+
+        <h4>Episode: ${curr.episode}</h4>
+        <h4>Url: ${curr.url}</h4>
+        </div>
+           `
+    }, "")
+    nextEpisode.onclick = () =>{
+        paginaActualEpisode ++
+        if(paginaActualEpisode > ultimaPaginaEpisode){
+            nextEpisode.disabled = true
+        }
+          if(paginaActualEpisode <= ultimaPaginaEpisode){
+            prevEpisode.disabled = false
+        } 
+        
+    buscarEpisode();
+    }
+    
+    prevEpisode.onclick = () =>{
+        paginaActualEpisode --
+       if(paginaActualEpisode < 1){
+           prevEpisode.disabled = true
+        }
+        if(paginaActualEpisode >= 1){
+            prevEpisode.disabled = false
+        }
+        buscarEpisode();
+    }
+
+
+    episode.innerHTML = htmlEpisode
+}
+
+buscarEpisode()
+
+//*****************PAGINACION PERSONAJES**********************/
+next.onclick = () =>{
+    paginaActual ++
+    if(paginaActual > ultimaPagina){
+        next.disabled = true
+    }
+      if(paginaActual <= ultimaPagina){
+        prev.disabled = false
+    } 
+    
+buscarPersonajes();
+}
+
+prev.onclick = () =>{
+    paginaActual --
+   if(paginaActual < 1){
+       prev.disabled = true
+    }
+    if(paginaActual >= 1){
+        prev.disabled = false
+    }
+    buscarPersonajes();
+}
+//****************************************************/
 
 
 
-//******************************peliculas******************** */
-
-//const buscarPeliculas = () =>{
-//    fetch(`https://rickandmortyapi.com/api/episode?page=${paginaActual}`)
-//    .then(res => res.json())
-//    .then(data =>{ 
-//        //console.log(data)
-//        ultimaPagina = data.info.pages
-//        mostrarHTMLPeliculas(data.results)
-//        //clickTarjetaPersonaje()
-//    })
-//}
 
 
-
-
-//const  mostrarHTMLPeliculas = (peliculas) =>{
-//    const pelicula = document.querySelector("#pelicula")
-//    const html = peliculas.reduce((acc, curr) => {
-//        return acc + `
-//        <div class ="tarjetas-peliculas" data-id=${curr.id}>
-//        <h2>${curr.name}</h2>
-//        <h2>${curr.url}<h2/>
-//        </div>
-//        `
-//    }, "")
-//    pelicula.innerHTML = html
-//}
-//buscarPeliculas()
 
 
 
